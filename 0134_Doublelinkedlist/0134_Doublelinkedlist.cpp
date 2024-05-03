19c1584359dcfbf6f95e81cc35c41a73bc44920d
spongebar-netizen <fatanjrz1@gmail.com>
2024-05-03 10:13:12 +0700

    return;

--- 0134_Doublelinkedlist/0134_Doublelinkedlist.cpp
+++ 0134_Doublelinkedlist/0134_Doublelinkedlist.cpp
@@ -22,3 +22,13 @@ void addNode() {
 
 		if (START != NULL && newNode->noMhs == START->noMhs) {
 			cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
+			return;
+		}
+		newNode->next = START;
+		if (START != NULL) {
+			START->prev = newNode;
+		}
+		newNode->prev = NULL;
+		START = newNode;
+	}
+
