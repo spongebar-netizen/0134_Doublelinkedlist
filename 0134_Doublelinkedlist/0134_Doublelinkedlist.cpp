f7a11f46abfcf34f156c50e711c501eec13a85f6
spongebar-netizen <fatanjrz1@gmail.com>
2024-05-03 10:12:40 +0700

    void addNode() {

--- 0134_Doublelinkedlist/0134_Doublelinkedlist.cpp
+++ 0134_Doublelinkedlist/0134_Doublelinkedlist.cpp
@@ -9,5 +9,16 @@ struct Node
 	Node* next;
 	Node* prev;
 };
+Node* START = NULL;
 
+void addNode() {
+	Node* newNode = new Node();
+	cout << "\nEnter the roll number of the student: ";
+	cin >> newNode->noMhs;
+	cout << "\nEnter the name of the student: ";
+	cin >> newNode->name;
 
+	if (START == NULL || newNode->noMhs <= START->noMhs) {
+
+		if (START != NULL && newNode->noMhs == START->noMhs) {
+			cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
