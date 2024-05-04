a80e714b140bf30cc253ad4d1b630e38ce26741d
spongebar-netizen <fatanjrz1@gmail.com>
2024-05-04 23:09:00 +0700

    noMhs

--- 0134_Doublelinkedlist/0134_Doublelinkedlist.cpp
+++ 0134_Doublelinkedlist/0134_Doublelinkedlist.cpp
@@ -139,8 +139,10 @@ void traverse()
 		Node* currentNode = START;
 		while (currentNode != NULL)
 		{
+
 			cout << currentNode->noMhs << " " << currentNode->name << endl;
 			currentNode = currentNode->next;
+
 		}
 	}
 }
